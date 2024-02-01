5c4f2fe050a7d44652852c50aef4a62c8c2f5ad3
iliana <ilianameit@gmail.com>
2024-02-01 22:39:17 +0300

    декомпозиция

---
+++ src/components/cards-list/cards-list.tsx
@@ -0,0 +1,11 @@
+import CardItem from '../card-item/card-item';
+
+function CardsList(): JSX.Element {
+  return(
+    <div className="cards catalog__cards">
+      <CardItem />
+    </div>
+  );
+}
+
+export default CardsList;
