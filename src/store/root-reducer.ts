399862824948d6553f7797d8ffdc93b833d41c96
iliana <ilianameit@gmail.com>
2024-02-15 21:31:02 +0300

    slice review

--- src/store/root-reducer.ts
+++ src/store/root-reducer.ts
@@ -1,7 +1,9 @@
 import { combineReducers } from '@reduxjs/toolkit';
 import { NameSpace } from '../const/const';
 import { camerasSlice } from './camera-slice/camera-slice';
+import { reviewSlice } from './review/review-slice';
 
 export const rootReducer = combineReducers({
   [NameSpace.Camera]: camerasSlice.reducer,
+  [NameSpace.Review]: reviewSlice.reducer,
 });
