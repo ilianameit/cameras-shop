a0ef29f73a69e4f8b68c39977d5fef96dbd9795b
iliana <ilianameit@gmail.com>
2024-04-02 16:51:17 +0300

    Добавила попап успешное добавление  камеры в корзину

--- src/components/popup/add-item-seccess-popup/add-item-seccess-popup.tsx
+++ src/components/popup/add-item-seccess-popup/add-item-seccess-popup.tsx
@@ -1,24 +1,56 @@
-function AddItemSeccessPopup(): JSX.Element {
+import { Fragment, MouseEvent, RefObject, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { AppRoutes } from '../../../const/const';
+
+type AddItemSeccessPopupProps = {
+  focusElement: RefObject<HTMLAnchorElement>;
+  onContinueButtonClick: () => void;
+  isCardItem: boolean;
+}
+
+function AddItemSeccessPopup({focusElement, onContinueButtonClick, isCardItem}: AddItemSeccessPopupProps): JSX.Element {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (focusElement && focusElement.current) {
+      focusElement.current.focus();
+    }
+  }, [focusElement]);
+
+  function handleReturnButtonClick() {
+    onContinueButtonClick();
+    navigate(AppRoutes.Basket);
+  }
+
+  function handleContinueButtonClick(evt: MouseEvent<HTMLAnchorElement>) {
+    evt.preventDefault();
+    onContinueButtonClick();
+    if(isCardItem) {
+      navigate(AppRoutes.Root);
+    }
+  }
   return(
-    <div className="modal is-active modal--narrow">
-      <div className="modal__wrapper">
-        <div className="modal__overlay"></div>
-        <div className="modal__content">
-          <p className="title title--h4">Товар успешно добавлен в корзину</p>
-          <svg className="modal__icon" width="86" height="80" aria-hidden="true">
-            <use xlinkHref="#icon-success"></use>
-          </svg>
-          <div className="modal__buttons"><a className="btn btn--transparent modal__btn" href="#">Продолжить покупки</a>
-            <button className="btn btn--purple modal__btn modal__btn--fit-width">Перейти в корзину</button>
-          </div>
-          <button className="cross-btn" type="button" aria-label="Закрыть попап" data-testid='close-button'>
-            <svg width="10" height="10" aria-hidden="true">
-              <use xlinkHref="#icon-close"></use>
-            </svg>
-          </button>
-        </div>
+    <Fragment>
+      <svg className="modal__icon" width="86" height="80" aria-hidden="true">
+        <use xlinkHref="#icon-success"></use>
+      </svg>
+      <div className="modal__buttons">
+        <a
+          className="btn btn--transparent modal__btn"
+          ref={focusElement}
+          onClick={handleContinueButtonClick}
+          href='#'
+        >
+          Продолжить покупки
+        </a>
+        <button
+          className="btn btn--purple modal__btn modal__btn--fit-width"
+          onClick={handleReturnButtonClick}
+        >
+          Перейти в корзину
+        </button>
       </div>
-    </div>
+    </Fragment>
   );
 }
 
