9dc51c28c3d5840ef3f389598d97275373efc663
iliana <ilianameit@gmail.com>
2024-04-02 16:55:49 +0300

    состояние кнопки для корзины

--- src/components/card-item/default-card-item.tsx
+++ src/components/card-item/default-card-item.tsx
@@ -4,19 +4,24 @@ import RatingStarsList from '../rating-stars-list/rating-stars-list';
 import { returnFormatedPrice } from '../../utils/common';
 import { AppRoutes } from '../../const/const';
 import { memo } from 'react';
+import { useAppSelector } from '../../hooks';
+import { getCamerasFromCart } from '../../store/camera-slice/selectors';
 
 type DefaultCardItemProps = {
   camera: Camera;
   onBuyClick: () => void;
 }
 function DefaultCardItemComponent({camera, onBuyClick}: DefaultCardItemProps): JSX.Element {
-  const {id, previewImgWebp, previewImgWebp2x, previewImg, previewImg2x, name, rating, reviewCount, price} = camera;
+  const {id, previewImgWebp, previewImgWebp2x, previewImg, previewImg2x, name, category, rating, reviewCount, price} = camera;
+  const camerasInCart = useAppSelector(getCamerasFromCart);
+  const isCameraInCart = camerasInCart.find((cameraInCart) => cameraInCart.id === id);
+
   return(
     <>
       <div className="product-card__img">
         <picture>
           <source type="image/webp" srcSet={`/${previewImgWebp}, /${previewImgWebp2x} 2x`}/>
-          <img src={`/${previewImg}`} srcSet={`/${previewImg2x} 2x`} width={280} height={240} alt={name} />
+          <img src={`/${previewImg}`} srcSet={`/${previewImg2x} 2x`} width={280} height={240} alt={`${category} «${name}»`} />
         </picture>
       </div>
       <div className="product-card__info">
@@ -30,20 +35,25 @@ function DefaultCardItemComponent({camera, onBuyClick}: DefaultCardItemProps): J
         </p>
       </div>
       <div className="product-card__buttons">
-        <button
-          className="btn btn--purple product-card__btn"
-          type="button"
-          onClick={onBuyClick}
-        >
-          Купить
-        </button>
         {
-          // <a className="btn btn--purple-border product-card__btn product-card__btn--in-cart" href="#">
-          //   <svg width="16" height="16" aria-hidden="true">
-          //     <use xlinkHref="#icon-basket"></use>
-          //   </svg>В корзине
-          // </a>
+          !isCameraInCart ?
+            <button
+              className="btn btn--purple product-card__btn"
+              type="button"
+              onClick={onBuyClick}
+            >
+              Купить
+            </button> :
+            <Link
+              className="btn btn--purple-border product-card__btn product-card__btn--in-cart"
+              to={AppRoutes.Basket}
+            >
+              <svg width="16" height="16" aria-hidden="true">
+                <use xlinkHref="#icon-basket"></use>
+              </svg>В корзине
+            </Link>
         }
+
         <Link className="btn btn--transparent" to={`${AppRoutes.Product}${id}`}>Подробнее
         </Link>
       </div>
