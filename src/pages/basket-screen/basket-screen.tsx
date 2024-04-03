e61e2d13e4c295f6606d5a2c0464666c64176ed6
iliana <ilianameit@gmail.com>
2024-04-04 01:37:45 +0300

    Добавила функции и модальное око отправки заказа

--- src/pages/basket-screen/basket-screen.tsx
+++ src/pages/basket-screen/basket-screen.tsx
@@ -5,14 +5,19 @@ import { Breadcrumb, Camera, CameraBasket } from '../../types/types';
 import { AppRoutes, ChangeProductCount, breadcrumbBasket, breadcrumbNames } from '../../const/const';
 import Breadcrumbs from '../../components/breadcrumbs/breadcrumbs';
 import { useAppDispatch, useAppSelector } from '../../hooks';
-import { getCamerasFromCart } from '../../store/camera-slice/selectors';
+import { getCamerasFromCart, getCreateOrderFailStatus, getCreateOrderSuccessStatus } from '../../store/camera-slice/selectors';
 import BasketProductItem from '../../components/basket-product-item/basket-product-item';
-import { changeCountCameraInBasket, deleteFromCart } from '../../store/camera-slice/camera-slice';
+import { changeCountCameraInBasket, closeErrorModal, deleteFromCart, resetCart } from '../../store/camera-slice/camera-slice';
 import { ChangeEvent, Fragment, MouseEvent, useEffect, useRef, useState } from 'react';
 import ModalWindow from '../../components/modal-window/modal-window';
 import RemoveItemPopup from '../../components/popup/remove-item-popup/remove-item-popup';
 import { Link, useNavigate } from 'react-router-dom';
 import { returnFormatedPrice } from '../../utils/common';
+import BasketPromo from '../../components/basket-promo/basket-promo';
+import { getPromocode } from '../../store/promo-slice/selectors';
+import { fetchSendOrder } from '../../store/api-actions';
+import { resetPromocode } from '../../store/promo-slice/promo-slice';
+import ThanksPopup from '../../components/popup/thanks-popup/thanks-popup';
 
 function BasketScreen(): JSX.Element {
   const dispatch = useAppDispatch();
@@ -29,6 +34,14 @@ function BasketScreen(): JSX.Element {
 
   const totalPriceCameras = cameras.reduce((prev, current) => prev + (current.price * current.count), 0);
   const [totalPrice, setTotalPrice] = useState<number>(totalPriceCameras);
+  const promocode = useAppSelector(getPromocode);
+
+  const discount = promocode.discount ? totalPrice * (promocode.discount / 100) : 0;
+  const totalPriceWithDiscount = totalPrice - discount;
+
+  const focusThanksPopup = useRef<HTMLButtonElement>(null);
+  const isSuccessCreateOrder = useAppSelector(getCreateOrderSuccessStatus);
+  const isFailCreateOrder = useAppSelector(getCreateOrderFailStatus);
 
   useEffect(() => {
     setTotalPrice(totalPriceCameras);
@@ -64,7 +77,29 @@ function BasketScreen(): JSX.Element {
   }
 
   function handleCountChange(event: ChangeEvent<HTMLInputElement>, id: CameraBasket['id']) {
-    dispatch(changeCountCameraInBasket({type: ChangeProductCount.SetCount, id, count: Number(event.target.value) }));
+    if(event.target.value.length){
+      dispatch(changeCountCameraInBasket({type: ChangeProductCount.SetCount, id, count: Number(event.target.value) }));
+    } else {
+      dispatch(changeCountCameraInBasket({type: ChangeProductCount.SetCount, id, count: 1 }));
+    }
+  }
+
+  function handleOrderButtonClick() {
+    const camerasIds = cameras.map((camera) => camera.id);
+    dispatch(fetchSendOrder({
+      camerasIds: camerasIds,
+      coupon: promocode.coupon
+    }));
+  }
+
+  function handleCloseThanksOrderModalClick() {
+    if(isSuccessCreateOrder){
+      dispatch(resetCart());
+    }
+    if(isFailCreateOrder){
+      dispatch(closeErrorModal());
+    }
+    dispatch(resetPromocode());
   }
 
   return(
@@ -100,30 +135,27 @@ function BasketScreen(): JSX.Element {
                   </Fragment>
               }
               <div className="basket__summary">
-                <div className="basket__promo">
-                  <p className="title title--h4">Если у вас есть промокод на скидку, примените его в этом поле</p>
-                  <div className="basket-form">
-                    <form action="#">
-                      <div className="custom-input">
-                        <label><span className="custom-input__label">Промокод</span>
-                          <input type="text" name="promo" placeholder="Введите промокод" />
-                        </label>
-                        <p className="custom-input__error">Промокод неверный</p>
-                        <p className="custom-input__success">Промокод принят!</p>
-                      </div>
-                      <button className="btn" type="submit">Применить
-                      </button>
-                    </form>
-                  </div>
-                </div>
+                <BasketPromo />
                 <div className="basket__summary-order">
                   <p className="basket__summary-item">
                     <span className="basket__summary-text">Всего:</span>
                     <span className="basket__summary-value">{returnFormatedPrice(totalPrice)}</span>
                   </p>
-                  <p className="basket__summary-item"><span className="basket__summary-text">Скидка:</span><span className="basket__summary-value basket__summary-value--bonus">0 ₽</span></p>
-                  <p className="basket__summary-item"><span className="basket__summary-text basket__summary-text--total">К оплате:</span><span className="basket__summary-value basket__summary-value--total">111 390 ₽</span></p>
-                  <button className="btn btn--purple" type="submit">Оформить заказ
+                  <p className="basket__summary-item">
+                    <span className="basket__summary-text">Скидка:</span>
+                    <span className="basket__summary-value basket__summary-value--bonus">{returnFormatedPrice(discount)}</span>
+                  </p>
+                  <p className="basket__summary-item">
+                    <span className="basket__summary-text basket__summary-text--total">К оплате:</span>
+                    <span className="basket__summary-value basket__summary-value--total">{returnFormatedPrice(totalPriceWithDiscount)}</span>
+                  </p>
+                  <button
+                    className="btn btn--purple"
+                    type="submit"
+                    onClick={handleOrderButtonClick}
+                    disabled={cameras.length === 0}
+                  >
+                    Оформить заказ
                   </button>
                 </div>
               </div>
@@ -143,6 +175,22 @@ function BasketScreen(): JSX.Element {
               focusElement={focusItemRemovePopup}
               onDeleteButtonClick={handleDeleteButtonClick}
               onContinueButtonClick={handleContinueButtonClick}
+            />
+          </ModalWindow>)
+      }
+      {
+        (isSuccessCreateOrder || isFailCreateOrder) && (
+          <ModalWindow
+            title={isFailCreateOrder ? 'Не удалось отправить данные' : 'Спасибо за покупку'}
+            onClose={handleCloseThanksOrderModalClick}
+            firstFocusElement={focusThanksPopup}
+            isResponse
+          >
+            <ThanksPopup
+              focusElement={focusThanksPopup}
+              isModalOrder
+              onClickContinue={handleCloseThanksOrderModalClick}
+              isErrorCreateOrder={isFailCreateOrder}
             />
           </ModalWindow>)
       }
