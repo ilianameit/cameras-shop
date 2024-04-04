14c43805be6307696809bea7b189226d2a146714
iliana <ilianameit@gmail.com>
2024-04-04 18:09:00 +0300

    Обновила моки для тестов

--- src/utils/mocks.ts
+++ src/utils/mocks.ts
@@ -1,7 +1,7 @@
 import { ThunkDispatch } from '@reduxjs/toolkit';
 import { Action } from 'redux';
 import { State } from '../types/state';
-import { Camera, Item, Review } from '../types/types';
+import { Camera, CameraBasket, Item, Review } from '../types/types';
 import { createAPI } from '../services/api';
 
 export const mockCamera: Camera = {
@@ -110,21 +110,87 @@ export const mockPromo: Item[] = [
   }
 ];
 
+export const mockCamerasBasket: CameraBasket[] = [
+  {
+    'id': 1,
+    count: 1,
+    'name': 'Ретрокамера Dus Auge lV',
+    'vendorCode': 'DA4IU67AD5',
+    'type': 'Коллекционная',
+    'category': 'Видеокамера',
+    'description': 'Немецкий концерн BRW разработал видеокамеру Das Auge IV в начале 80-х годов, однако она до сих пор пользуется популярностью среди коллекционеров и яростных почитателей старинной техники. Вы тоже можете прикоснуться к волшебству аналоговой съёмки, заказав этот чудо-аппарат. Кто знает, может с Das Auge IV начнётся ваш путь к наградам всех престижных кинофестивалей.',
+    'previewImg': 'img/content/das-auge.jpg',
+    'level': 'Любительский',
+    'price': 73450,
+    'previewImg2x': 'img/content/das-auge@2x.jpg',
+    'previewImgWebp': 'img/content/das-auge.webp',
+    'previewImgWebp2x': 'img/content/das-auge@2x.webp',
+    'rating': 4,
+    'reviewCount': 165
+  },
+  {
+    'id': 2,
+    count: 2,
+    'name': 'FastShot MR-5',
+    'vendorCode': 'JH34KHN895',
+    'type': 'Моментальная',
+    'category': 'Фотоаппарат',
+    'description': 'Новое слово в создании моментальных фото. Высокое качество снимков, легко перезаряжаемые кассеты, встроенная вспышка. Создавайте альбомы здесь и сейчас.',
+    'previewImg': 'img/content/fast-shot.jpg',
+    'level': 'Любительский',
+    'price': 18970,
+    'previewImg2x': 'img/content/fast-shot@2x.jpg',
+    'previewImgWebp': 'img/content/fast-shot.webp',
+    'previewImgWebp2x': 'img/content/fast-shot@2x.webp',
+    'rating': 4,
+    'reviewCount': 153
+  },
+  {
+    'id': 3,
+    count: 1,
+    'name': 'Instaprinter P2',
+    'vendorCode': 'KLU789GH56',
+    'type': 'Цифровая',
+    'category': 'Фотоаппарат',
+    'description': 'Компактная модель позволяющая получать чёткие снимки с 25-кратным зумом. В комплекте зарядное устройство и бархатный чехол, а так же удобный шнурок на шею.',
+    'previewImg': 'img/content/instaprinter.jpg',
+    'level': 'Нулевой',
+    'price': 8430,
+    'previewImg2x': 'img/content/instaprinter@2x.jpg',
+    'previewImgWebp': 'img/content/instaprinter.webp',
+    'previewImgWebp2x': 'img/content/instaprinter@2x.webp',
+    'rating': 3,
+    'reviewCount': 95
+  }
+];
+
 export const MockStore = (initialState?: Partial<State>): State => ({
   CAMERA: {
     cameras: [],
-    promo: [],
     loadingCameras: false,
     oneCamera: mockCamera,
     loadingOneCamera: false,
     similarCameras: [],
     camerasFilteredByPrice: [],
     camerasFilteredByPriceLoading: false,
+    cart: [],
+    isSuccessAddToCart: false,
+    isCreateOrderSuccess: false,
+    isCreateOrderFail: false,
   },
   REVIEW: {
     reviews: [],
     isLoadingReview: false,
-    isAddReviewSucces: false
+    isAddReviewSuccess: false
+  },
+  PROMO: {
+    promo: [],
+    promocode: {
+      coupon: null,
+      discount: 0
+    },
+    isDiscountLoading: false,
+    invalidCoupon: false
   },
   ...initialState ?? {},
 });
