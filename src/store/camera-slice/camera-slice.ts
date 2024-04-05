2b5806170738143f8ca70eaf625a7cee056ec987
iliana <ilianameit@gmail.com>
2024-04-05 05:21:22 +0300

    создала слайд для корзины

--- src/store/camera-slice/camera-slice.ts
+++ src/store/camera-slice/camera-slice.ts
@@ -1,8 +1,7 @@
-import { PayloadAction, createSlice } from '@reduxjs/toolkit';
-import { ChangeProductCount, MAX_COUNT_ITEM_BASKET, MIN_COUNT_ITEM_BASKET, NAME_KEY_CAMERAS_STORAGE, NameSpace } from '../../const/const';
-import { Camera, CameraBasket, ChangeCount} from '../../types/types';
-import { fetchCamerasAction, fetchCamerasPriceAction, fetchOneCameraAction, fetchSendOrder, fetchSimilarCamerasAction } from '../api-actions';
-import { getCamerasFromLocalStorage } from '../../utils/common';
+import { createSlice } from '@reduxjs/toolkit';
+import { NameSpace } from '../../const/const';
+import { Camera } from '../../types/types';
+import { fetchCamerasAction, fetchCamerasPriceAction, fetchOneCameraAction, fetchSimilarCamerasAction } from '../api-actions';
 
 type CamerasStateType = {
   cameras: Camera[];
@@ -12,10 +11,6 @@ type CamerasStateType = {
   similarCameras: Camera[];
   camerasFilteredByPrice: Camera[];
   camerasFilteredByPriceLoading: boolean;
-  cart: CameraBasket[];
-  isSuccessAddToCart: boolean;
-  isCreateOrderSuccess: boolean;
-  isCreateOrderFail: boolean;
 }
 
 const initialState: CamerasStateType = {
@@ -26,10 +21,6 @@ const initialState: CamerasStateType = {
   similarCameras: [],
   camerasFilteredByPrice: [],
   camerasFilteredByPriceLoading: false,
-  cart: JSON.parse(localStorage.getItem(NAME_KEY_CAMERAS_STORAGE) || '[]') as CameraBasket[],
-  isSuccessAddToCart: false,
-  isCreateOrderSuccess: false,
-  isCreateOrderFail: false,
 };
 
 export const camerasSlice = createSlice({
@@ -38,64 +29,6 @@ export const camerasSlice = createSlice({
   reducers: {
     dropCamera: (state) => {
       state.oneCamera = null;
-    },
-    changeStatusAddToCart: (state) => {
-      state.isSuccessAddToCart = !state.isSuccessAddToCart;
-    },
-    addToCart: (state, action: PayloadAction<Camera>) => {
-      const camerasInCart = state.cart;
-      const camera = action.payload;
-      const findDuplicateCameraIndex = camerasInCart.findIndex((cameraCart) => cameraCart.id === camera.id);
-
-      if(findDuplicateCameraIndex !== -1) {
-        const duplicateCamera = camerasInCart[findDuplicateCameraIndex];
-        duplicateCamera.count = duplicateCamera.count + 1;
-        state.cart.splice(findDuplicateCameraIndex, 1, duplicateCamera);
-        getCamerasFromLocalStorage(state.cart);
-      } else {
-        state.cart.push({...camera, count: 1});
-        getCamerasFromLocalStorage(state.cart);
-      }
-      state.isSuccessAddToCart = !state.isSuccessAddToCart;
-    },
-    deleteFromCart: (state, action: PayloadAction<Camera['id']>) => {
-      state.cart = state.cart.filter((product) => product.id !== action.payload);
-      getCamerasFromLocalStorage(state.cart);
-    },
-    changeCountCameraInBasket: (state, action: PayloadAction<ChangeCount>) => {
-      const payload = action.payload;
-      const newCount = payload.count;
-      const idCamera = payload.id;
-      const camerasInCart = state.cart;
-
-      const findCameraIndex = camerasInCart.findIndex((cameraCart) => cameraCart.id === idCamera);
-
-      if(findCameraIndex !== -1) {
-        let camera = camerasInCart[findCameraIndex];
-
-        if(newCount && payload.type === ChangeProductCount.SetCount){
-          if(MIN_COUNT_ITEM_BASKET <= newCount && newCount <= MAX_COUNT_ITEM_BASKET){
-            camera = {...camera, count: newCount};
-          }
-        }
-        if(payload.type === ChangeProductCount.Increase && camera.count < MAX_COUNT_ITEM_BASKET) {
-          camera = {...camera, count: ++camera.count};
-        }
-        if(payload.type === ChangeProductCount.Decrease && camera.count > MIN_COUNT_ITEM_BASKET){
-          camera = {...camera, count: --camera.count};
-        }
-
-        state.cart.splice(findCameraIndex, 1, camera);
-        getCamerasFromLocalStorage(state.cart);
-      }
-    },
-    resetCart: (state) => {
-      state.isCreateOrderSuccess = false;
-      state.cart = [];
-      getCamerasFromLocalStorage([]);
-    },
-    closeErrorModal: (state) => {
-      state.isCreateOrderFail = false;
     },
   },
   extraReducers(builder) {
@@ -125,16 +58,8 @@ export const camerasSlice = createSlice({
       })
       .addCase(fetchSimilarCamerasAction.fulfilled, (state, action) => {
         state.similarCameras = action.payload;
-      })
-      .addCase(fetchSendOrder.fulfilled, (state) => {
-        state.isCreateOrderSuccess = true;
-        state.isCreateOrderFail = false;
-      })
-      .addCase(fetchSendOrder.rejected, (state) => {
-        state.isCreateOrderSuccess = false;
-        state.isCreateOrderFail = true;
       });
   },
 });
 
-export const { dropCamera, addToCart, changeStatusAddToCart, deleteFromCart, changeCountCameraInBasket, resetCart, closeErrorModal } = camerasSlice.actions;
+export const { dropCamera } = camerasSlice.actions;
--- src/store/root-reducer.ts
+++ src/store/root-reducer.ts
@@ -3,9 +3,11 @@ import { NameSpace } from '../const/const';
 import { camerasSlice } from './camera-slice/camera-slice';
 import { reviewSlice } from './review/review-slice';
 import { promoSlice } from './promo-slice/promo-slice';
+import { basketSlice } from './basket-slice/basket-slice';
 
 export const rootReducer = combineReducers({
   [NameSpace.Camera]: camerasSlice.reducer,
   [NameSpace.Review]: reviewSlice.reducer,
   [NameSpace.Promo]: promoSlice.reducer,
+  [NameSpace.Basket]: basketSlice.reducer,
 });
